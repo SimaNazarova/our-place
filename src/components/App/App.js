420aa6100602ea19ec3c510e12ce56faacf0f61b
Serafima Nazarova <sima.nazarova@gmail.com>
2023-04-26 14:06:41 +0400

    add base

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className="header__up"></div>
+        <div className="header__bottom"></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
